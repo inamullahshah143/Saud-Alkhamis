7ba62a7ef797646ce49487a4e61f1dff986aeb2f
Inamullah Shah <89691107+inamullahshah143@users.noreply.github.com>
2021-11-23 19:13:36 +0500

    .

--- saeud_alkhamis/lib/views/home/home_tabs/blog.dart
+++ saeud_alkhamis/lib/views/home/home_tabs/blog.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:saeud_alkhamis/views/home/home_tabs/blog_subs/blog_view.dart';
 import 'package:saeud_alkhamis/views/widgets/const.dart';
 
+import 'dashboard_subs/filter_form.dart';
+
 class Blog extends StatefulWidget {
   const Blog({Key key}) : super(key: key);
 
@@ -31,7 +33,7 @@ class _BlogState extends State<Blog> {
             collapsedHeight: height * 0.25 - 125,
             elevation: 0,
             pinned: true,
-            leadingWidth: 200,
+            leadingWidth: 240,
             leading: Row(
               children: [
                 Padding(
@@ -86,7 +88,7 @@ class _BlogState extends State<Blog> {
             ),
             actions: [
               Padding(
-                padding: EdgeInsets.symmetric(horizontal: 10.0),
+                padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 3),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.end,
                   children: const [
@@ -111,73 +113,105 @@ class _BlogState extends State<Blog> {
               ),
             ],
             bottom: PreferredSize(
-              preferredSize: Size(MediaQuery.of(context).size.width, 125),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Image(
+              preferredSize: Size(MediaQuery.of(context).size.width, 130),
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      pagesColor.withOpacity(0),
+                      pagesColor.withOpacity(0.5),
+                      pagesColor,
+                    ],
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Image(
                       image: AssetImage('assets/images/logo.png'),
-                      width: 100,
+                      width: 90,
                     ),
-                  ),
-                  ChipsChoice<String>.multiple(
-                    value: tags,
-                    choiceActiveStyle: C2ChoiceStyle(
-                      showCheckmark: false,
-                      brightness: Brightness.dark,
-                      color: yellowFonts,
-                      borderRadius: BorderRadius.all(Radius.circular(25)),
-                      borderColor: pagesColor,
-                    ),
-                    choiceStyle: C2ChoiceStyle(
-                      showCheckmark: false,
-                      brightness: Brightness.dark,
-                      color: pagesColor,
-                      borderRadius: BorderRadius.all(Radius.circular(25)),
-                      borderColor: pagesColor,
-                    ),
-                    onChanged: (val) => setState(() => tags = val),
-                    choiceItems: C2Choice.listFrom<String, String>(
-                      source: options,
-                      value: (i, v) => v,
-                      label: (i, v) => v,
-                      tooltip: (i, v) => v,
-                      style: (i, v) {
-                        return C2ChoiceStyle(
-                          showCheckmark: false,
-                          borderRadius: BorderRadius.all(Radius.circular(25)),
-                          borderColor: pagesColor,
-                        );
-                      },
-                      activeStyle: (i, v) {
-                        return C2ChoiceStyle(
-                          showCheckmark: false,
-                          borderRadius: BorderRadius.all(Radius.circular(25)),
-                          borderColor: yellowFonts,
-                        );
-                      },
-                    ),
-                  ),
-                  ListTile(
-                    dense: true,
-                    leading: ImageIcon(
-                      AssetImage('assets/images/icons/filter.png'),
-                      size: 16,
-                      color: yellowFonts,
-                    ),
-                    trailing: Text(
-                      'آخر التحديثات',
-                      textDirection: TextDirection.rtl,
-                      style: TextStyle(
+                    ChipsChoice<String>.multiple(
+                      value: tags,
+                      choiceActiveStyle: C2ChoiceStyle(
+                        showCheckmark: false,
+                        brightness: Brightness.dark,
                         color: yellowFonts,
-                        fontSize: 12,
+                        borderRadius: BorderRadius.all(Radius.circular(25)),
+                        borderColor: pagesColor,
+                      ),
+                      choiceStyle: C2ChoiceStyle(
+                        showCheckmark: false,
+                        brightness: Brightness.dark,
+                        color: pagesColor,
+                        borderRadius: BorderRadius.all(Radius.circular(25)),
+                        borderColor: pagesColor,
+                      ),
+                      onChanged: (val) => setState(() => tags = val),
+                      choiceItems: C2Choice.listFrom<String, String>(
+                        source: options,
+                        value: (i, v) => v,
+                        label: (i, v) => v,
+                        tooltip: (i, v) => v,
+                        style: (i, v) {
+                          return C2ChoiceStyle(
+                            showCheckmark: false,
+                            borderRadius: BorderRadius.all(Radius.circular(25)),
+                            borderColor: pagesColor,
+                          );
+                        },
+                        activeStyle: (i, v) {
+                          return C2ChoiceStyle(
+                            showCheckmark: false,
+                            borderRadius: BorderRadius.all(Radius.circular(25)),
+                            borderColor: yellowFonts,
+                          );
+                        },
                       ),
                     ),
-                  ),
-                ],
+                    ListTile(
+                      dense: true,
+                      leading: IconButton(
+                        onPressed: () {
+                          showModalBottomSheet(
+                            isScrollControlled: true,
+                            enableDrag: true,
+                            isDismissible: false,
+                            backgroundColor: Colors.transparent,
+                            context: context,
+                            builder: (BuildContext context) => Container(
+                              height: MediaQuery.of(context).size.height * 0.4,
+                              width: MediaQuery.of(context).size.width,
+                              decoration: BoxDecoration(
+                                color: Colors.transparent,
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(25.0),
+                                  topRight: Radius.circular(25.0),
+                                ),
+                              ),
+                              child: FilterForm(),
+                            ),
+                          );
+                        },
+                        icon: ImageIcon(
+                          AssetImage('assets/images/icons/filter.png'),
+                          size: 16,
+                          color: yellowFonts,
+                        ),
+                      ),
+                      trailing: Text(
+                        'آخر التحديثات',
+                        textDirection: TextDirection.rtl,
+                        style: TextStyle(
+                          color: yellowFonts,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
             flexibleSpace: BackgroundFlexibleSpaceBar(
--- saeud_alkhamis/lib/views/home/home_tabs/blog_subs/blog_view.dart
+++ saeud_alkhamis/lib/views/home/home_tabs/blog_subs/blog_view.dart
@@ -2,6 +2,7 @@
 
 import 'package:background_app_bar/background_app_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:saeud_alkhamis/views/home/home_tabs/dashboard_subs/filter_form.dart';
 import 'package:saeud_alkhamis/views/widgets/const.dart';
 
 class BlogView extends StatefulWidget {
@@ -25,7 +26,7 @@ class _BlogViewState extends State<BlogView> {
             collapsedHeight: height * 0.25 - 125,
             elevation: 0,
             pinned: true,
-            leadingWidth: 200,
+            leadingWidth: 240,
             leading: Row(
               children: [
                 Padding(
@@ -97,68 +98,101 @@ class _BlogViewState extends State<BlogView> {
               ),
             ],
             bottom: PreferredSize(
-              preferredSize: Size(MediaQuery.of(context).size.width, 125),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Image(
+              preferredSize: Size(MediaQuery.of(context).size.width, 130),
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      pagesColor.withOpacity(0),
+                      pagesColor.withOpacity(0.5),
+                      pagesColor,
+                    ],
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Image(
                       image: AssetImage('assets/images/logo.png'),
-                      width: 100,
+                      width: 90,
                     ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Container(
-                          height: 40,
-                          width: width * 0.4,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(50),
-                            color: pagesColor,
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 10.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Container(
+                            height: 40,
+                            width: width * 0.4,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(50),
+                              color: pagesColor,
+                            ),
                           ),
-                        ),
-                        Container(
-                          height: 40,
-                          width: width * 0.4,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(50),
-                            color: pagesColor,
+                          Container(
+                            height: 40,
+                            width: width * 0.4,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(50),
+                              color: pagesColor,
+                            ),
+                            child: Row(
+                              children: [
+                                Text("مشاركة"),
+                                ImageIcon(
+                                  AssetImage(
+                                      'assets/images/icons/search-2.png'),
+                                  color: darkFonts,
+                                ),
+                              ],
+                            ),
                           ),
-                          child: Row(
-                            children: [
-                              Text("مشاركة"),
-                              ImageIcon(
-                                AssetImage('assets/images/icons/search-2.png'),
-                                color: darkFonts,
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  ListTile(
-                    dense: true,
-                    leading: ImageIcon(
-                      AssetImage('assets/images/icons/filter.png'),
-                      size: 16,
-                      color: yellowFonts,
-                    ),
-                    trailing: Text(
-                      'آخر التحديثات',
-                      textDirection: TextDirection.rtl,
-                      style: TextStyle(
-                        color: yellowFonts,
-                        fontSize: 12,
+                        ],
                       ),
                     ),
-                  ),
-                ],
+                    ListTile(
+                      dense: true,
+                      leading: IconButton(
+                        onPressed: () {
+                          showModalBottomSheet(
+                            isScrollControlled: true,
+                            enableDrag: true,
+                            isDismissible: false,
+                            backgroundColor: Colors.transparent,
+                            context: context,
+                            builder: (BuildContext context) => Container(
+                              height: MediaQuery.of(context).size.height * 0.4,
+                              width: MediaQuery.of(context).size.width,
+                              decoration: BoxDecoration(
+                                color: Colors.transparent,
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(25.0),
+                                  topRight: Radius.circular(25.0),
+                                ),
+                              ),
+                              child: FilterForm(),
+                            ),
+                          );
+                        },
+                        icon: ImageIcon(
+                          AssetImage('assets/images/icons/filter.png'),
+                          size: 16,
+                          color: yellowFonts,
+                        ),
+                      ),
+                      trailing: Text(
+                        'آخر التحديثات',
+                        textDirection: TextDirection.rtl,
+                        style: TextStyle(
+                          color: yellowFonts,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
             flexibleSpace: BackgroundFlexibleSpaceBar(
